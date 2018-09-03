9ec1f650be15c17add69ee6c17c39d47b6e14101
Ming Han <mxu007@e.ntu.edu.sg>
2018-09-03 00:10:48 -0400

    09/02/18

--- 234_Palindrome_Linked_List.py
+++ 234_Palindrome_Linked_List.py
@@ -27,7 +27,7 @@ class Solution:
         #print(result)
         return result == result[::-1]
 
-# 2) Recursive approach
+# 2) Recursive approach, O(N) compexity
 class Solution:
     def isPalindrome(self, head):
         """
@@ -46,3 +46,34 @@ class Solution:
         self.left = head
         result = recur_isPalindrome(head)
         return result
+
+# 3） use fast and slow pointers to find mid pos
+# from mid point, reverse the second half of the linked list
+# e.g. 1>2>3>2>1, second half 3>2>1 reversed as 1>2>3
+# compare from head (left-most) and node (right most)
+# O(N) time complexity, O(1) space
+class Solution:
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = slow = head
+        # find the mid node
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        # reverse the second half
+        node = None
+        while slow:
+            nxt = slow.next
+            slow.next = node
+            node = slow
+            slow = nxt
+        # compare the first and second half nodes
+        while node:
+            if node.val != head.val:
+                return False
+            node = node.next
+            head = head.next
+        return True
