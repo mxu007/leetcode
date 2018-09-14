67b6db2bba027bfe20d63e49adbfc1c570ff5800
Ming Han <mxu007@e.ntu.edu.sg>
2018-09-14 00:18:56 -0400

    09/13/18

--- 874_Walking_Robot_Simulation.py
+++ 874_Walking_Robot_Simulation.py
@@ -181,5 +181,48 @@ class Solution(object):
 
         return ans
 
+# 5) combine features of solution 3)and 4)
+class Solution:
+    def robotSim(self, commands, obstacles):
+        """
+        :type commands: List[int]
+        :type obstacles: List[List[int]]
+        :rtype: int
+        """
+        x, y, direction = 0, 0, 0
+        max_dist = 0
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
+        dict_x = {}
+        dict_y = {}
+
+        for obstacle in obstacles:
+            if obstacle[0] not in dict_x:
+                dict_x[obstacle[0]] = [obstacle[1]]
+            else:
+                dict_x[obstacle[0]].append(obstacle[1])
+
+            if obstacle[1] not in dict_y:
+                dict_y[obstacle[1]] = [obstacle[0]]
+            else:
+                dict_y[obstacle[1]].append(obstacle[0])
+
+        # direction 0: north, 1: east, 2: south, 3: west
+        for command in commands:
+            if command == -1:
+                direction = (direction + 1) % 4
+            elif command == -2:
+                direction = (direction - 1) % 4
+
+            else:
+                if (direction == 0 or direction == 2) and x not in dict_x: y += dy[direction] * command
+                elif (direction == 1 or direction == 3) and y not in dict_y: x += dx[direction] * command
+                else:
+                    while command:
+                        if (direction == 0 or direction == 2) and y+dy[direction] not in dict_x[x]: y = y + dy[direction]
+                        elif (direction == 1 or direction == 3) and x+dx[direction] not in dict_y[y]: x = x + dx[direction]
+                        command -= 1
+                if x**2 + y**2 > max_dist: max_dist = x**2 + y**2
+        return max_dist
 
 
