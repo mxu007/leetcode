439b45f8c58fa017fb9024ab1925dd3e38353c11
Ming Han <mxu007@e.ntu.edu.sg>
2018-09-14 23:05:32 -0400

    09/14/18

---
+++ 168_Excel_Sheet_Column_Title.py
@@ -0,0 +1,62 @@
+# Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+# For example:
+
+#     1 -> A
+#     2 -> B
+#     3 -> C
+#     ...
+#     26 -> Z
+#     27 -> AA
+#     28 -> AB
+#     ...
+# Example 1:
+
+# Input: 1
+# Output: "A"
+# Example 2:
+
+# Input: 28
+# Output: "AB"
+# Example 3:
+
+# Input: 701
+# Output: "ZY"
+
+# https://leetcode.com/problems/excel-sheet-column-title/description/
+
+# 1) handle three cases, logN time complexity
+import collections
+import string
+class Solution:
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        num_to_alpha = collections.defaultdict()
+        result = ""
+        for i, strg in enumerate(string.ascii_uppercase):
+            num_to_alpha[i+1] = strg
+        num_to_alpha[0] = 'Z'
+
+        j = 1
+        while n > 0:
+            # if n is multiple of 26 and greater than max val of current index, means we need a additional alphabet to the left
+            if n % 26 == 0 and n > 26**j:
+                # get remainder
+                result = num_to_alpha[n%(26**j)//(26**(j-1))] + result
+                # subtract n
+                n = n - (n%(26**j)) if n%(26**j) else n - 26**j
+            # if n is multiple of 26 and smaller than max val of current index
+            elif n%26 == 0 and n <= 26**j:
+                result = num_to_alpha[n//26**(j-1)] + result
+                n -= 26 ** j
+            # if n is not the multiple of 26
+            else:
+                result = num_to_alpha[n%26] + result
+                n -= (26 ** (j-1))* (n%26)
+            j += 1
+        return result
+
+
