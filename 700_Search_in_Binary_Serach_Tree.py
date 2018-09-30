8b79e36479f44b5f4790b3a76945a9feddffd3bf
Ming Han <mxu007@e.ntu.edu.sg>
2018-09-29 23:58:04 -0400

    09/28/18

--- 700_Search_in_Binary_Serach_Tree.py
+++ 700_Search_in_Binary_Serach_Tree.py
@@ -29,6 +29,7 @@
 # https://leetcode.com/problems/search-in-a-binary-search-tree/description/
 # https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
 
+# 1) binary search, O(log(N)) time for height balanced tree
 class Solution(object):
     def searchBST(self, root, val):
         """
@@ -48,3 +49,80 @@ class Solution(object):
             return self.searchBST(root.right, val)
         # search left child of root if target val is smaller than root.val
         return self.searchBST(root.left, val)
+
+# 2) variant of 1)
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root and val < root.val:
+            return self.searchBST(root.left, val)
+        elif root and val > root.val:
+            return self.searchBST(root.right, val)
+        return root
+
+# 3）iterative solution by moving the head/root based on the value comparing with val
+# O(N) time
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root and val < root.val:
+            return self.searchBST(root.left, val)
+        elif root and val > root.val:
+            return self.searchBST(root.right, val)
+        return root
+
+# 4) use stack to store the nodes, last-in first-out properties to simulate the recursive call
+# O(N) time for worst-case, average O(log(N)) time
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        stack = [root]
+        while(len(stack)>0 and root):
+            root = stack.pop()
+            if root and root.val == val:
+                return root
+            elif root and root.val < val:
+                stack.append(root.right)
+            elif root and root.val > val:
+                stack.append(root.left)
+        return root
+
+# 5) Morris Traversal O(N) time and O(1) space
+# https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion-and-without-stack/
+class Solution:
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        current, pre = root, None
+        while current:
+            print(current.val)
+            if current.val == val:
+                return current
+            if not current.left:
+                current = current.right
+            else:
+                pre = current.left
+                while pre.right and pre.right != current:
+                    pre = pre.right
+                if not pre.right:
+                    pre.right = current
+                    current = current.left
+                else:
+                    pre.right = None
+                    current = current.right
+        return None
